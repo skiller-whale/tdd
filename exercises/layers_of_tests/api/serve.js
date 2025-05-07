0ccdaa296156036184a94ee9cd9ab4ec4d68869c
Amyas Merivale <merivale@gmail.com>
2025-05-07 15:24:23 +0200

    Remove layers of tests exercises

--- exercises/layers_of_tests/api/serve.js
+++
@@ -1,8 +0,0 @@
-import router from "./router.js";
-import * as dao from "./dao/dao.js";
-
-const port = 8082;
-const fetch = (request) => router(dao, request);
-
-Bun.serve({ port, fetch });
-console.log(`API server listening on port ${port}.`)
--- exercises/layers_of_tests/db/serve.js
+++
@@ -1,9 +0,0 @@
-import { Database } from "bun:sqlite";
-import router from "./router.js";
-
-const db = new Database("./db.sqlite");
-const port = 8081;
-const fetch = (request) => router(db, request);
-
-Bun.serve({ port, fetch });
-console.log(`DB server listening on port ${port}.`)
